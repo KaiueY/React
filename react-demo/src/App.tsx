9222b8c8ff4002ba1cab1a3e6fec9fae07d01237
Raphael Kai <169074373+KaiueY@users.noreply.github.com>
2025-01-26 18:54:33 +0800

    feat:优化

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -2,29 +2,36 @@ import Card from "./components/Card";
 import './components/Message/index'; // 确保导入 Message 组件
 
 function App() {
-  const showMessages = () => {
-    window.Message?.success('操作成功！');
+  const showErrorMessages = () ｛
     setTimeout(() => {
       window.Message?.error('发生错误！');
     }, 1000);
-    setTimeout(() => {
-      window.Message?.warning('注意事项！');
-    }, 2000);
-    setTimeout(() => {
-      window.Message?.info('提示信息');
-    }, 3000);
   };
-
+  const showSuccessMessages = () ｛
+    setTimeout(() => {
+      window.Message?.success('操作成功！');
+    }, 1000);
+  };
+  const showTipMessages = () ｛
+    setTimeout(() => {
+      window.Message?.warning('信息提示！');
+    }, 1000);
+  };
   return (
     <div style={{ padding: '20px' }}>
       <Card />
       <div style={{ marginTop: '20px' }}>
-        <button onClick={showMessages}>
-          显示所有消息类型
+        <button onClick={showErrorMessages}>
+          错误提示
+        </button>
+                <button onClick={showSuccessMessages}>
+          提示信息
+        </button>
+                <button onClick={showTipMessages}>
+          成功信息
         </button>
       </div>
       <button onClick={() => window.Message.success('成功提示')}>
-        显示成功消息
       </button>
     </div>
   );
